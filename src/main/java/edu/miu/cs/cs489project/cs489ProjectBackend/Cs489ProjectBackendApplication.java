2d3d0e5ed1418a59081d95f2df242e92db9c1dc9
Abdoulllahi <dabdoulahi@gmail.com>
2024-04-16 11:49:37 -0500

    init

---
+++ src/main/java/edu/miu/cs/cs489project/cs489ProjectBackend/Cs489ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489project.cs489ProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs489ProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs489ProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/cs489project/cs489ProjectBackend/Cs489ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489project.cs489ProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs489ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
